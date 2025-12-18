ca301c6bce3f79ac6ee572cee80305ddd1753af9
jaykant01 <jaykanty24092004@gmail.com>
2025-12-18 20:12:12 +0530

    [Jaykant] Add Problem 1

---
+++ Level2/Problem1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2
+{
+    internal class Problem1 
+    {
+        public void Calculate(int num1, int num2)
+        {
+            int quot = num1 / num2;
+            int rem = num1 % num2;
+
+            Console.WriteLine($"The Quotient is {quot} and Remainder is {rem} of two numbers {num1} and {num2}");
+        }
+        
+    }
+}
---
+++ Level2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Level2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Problem 1
+        Console.WriteLine("Enter num1 & num2: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Problem1 p1 = new Problem1();
+        p1.Calculate(num1, num2);
+
+
+        Console.ReadKey();
+    }
+}
