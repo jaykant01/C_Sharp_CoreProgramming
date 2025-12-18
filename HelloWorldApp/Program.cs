8f05b0db273b45e4d8a98b1094680ecdd4c387f0
jaykant01 <jaykanty24092004@gmail.com>
2025-12-18 18:26:08 +0530

    [Jaykant] Fixed .My First code in C sharp

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello C#");
+        Console.WriteLine("This is My First Programm");
+        // This below line is written to stop the console
+        Console.ReadLine();
     }
 }
