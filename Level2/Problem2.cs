d87446ff9b20ce3218481e0854c95664f7d7726d
jaykant01 <jaykanty24092004@gmail.com>
2025-12-18 22:30:15 +0530

    [Jaykant] Add Problem 2

---
+++ Level2/Problem2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2
+{
+    internal class Problem2
+    {
+        public void Precedence(int num1, int num2, int num3)
+        {
+            int op1 = num1 + num2 * num3;
+            int op2 = num1 * num2 + num3;
+            int op3 = num3 + num1 / num2;
+
+            Console.WriteLine($"The results of Int Operations are {op1}, {op2}, and {op3}");
+        }
+
+    }
+}
--- Level2/Program.cs
+++ Level2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Problem 1
+        ////Problem 1
         Console.WriteLine("Enter num1 & num2: ");
         int num1 = Convert.ToInt32(Console.ReadLine());
         int num2 = Convert.ToInt32(Console.ReadLine());
@@ -13,6 +13,15 @@ class Program
         p1.Calculate(num1, num2);
 
 
-        Console.ReadKey();
+        //Console.ReadKey();
+
+        // Problem 2
+        Console.WriteLine("Enter a, b & c: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        Problem2 p2 = new Problem2();
+        p2.Precedence(a, b, c);
     }
 }
