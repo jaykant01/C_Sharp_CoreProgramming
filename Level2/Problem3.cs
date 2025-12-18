42d99df22bd935e339aaa0f814cf07b89a6fa83b
jaykant01 <jaykanty24092004@gmail.com>
2025-12-18 22:33:45 +0530

    [Jaykant] Add Problem 3

---
+++ Level2/Problem3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2
+{
+    internal class Problem3
+    {
+        public void DoubleOpt(double x, double y, double z)
+        {
+            double op1 = x + y * z;
+            double op2 = x * y + z;
+            double op3 = z + x / y;
+
+            Console.WriteLine($"The results of Int Operations are {op1}, {op2}, and {op3}");
+        }
+
+    }
+}
--- Level2/Program.cs
+++ Level2/Program.cs
@@ -4,24 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        ////Problem 1
-        Console.WriteLine("Enter num1 & num2: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
+        // Problem 1
+        //Console.WriteLine("Enter num1 & num2: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Problem1 p1 = new Problem1();
-        p1.Calculate(num1, num2);
+        //Problem1 p1 = new Problem1();
+        //p1.Calculate(num1, num2);
 
-
-        //Console.ReadKey();
 
         // Problem 2
-        Console.WriteLine("Enter a, b & c: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        int b = Convert.ToInt32(Console.ReadLine());
-        int c = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter a, b & c: ");
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //int c = Convert.ToInt32(Console.ReadLine());
 
-        Problem2 p2 = new Problem2();
-        p2.Precedence(a, b, c);
+        //Problem2 p2 = new Problem2();
+        //p2.Precedence(a, b, c);
+
+        // Problem 3
+        Console.WriteLine("Enter x, y & z");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+        double z = Convert.ToDouble(Console.ReadLine());
+
+        Problem3 p3 = new Problem3();
+        p3.DoubleOpt(x, y, z);
+
+        Console.ReadKey();
     }
 }
