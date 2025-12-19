7ca66e85d7f61fbc37d3a7e1bdbbdd6e2ad6617b
jaykant01 <jaykanty24092004@gmail.com>
2025-12-19 19:51:46 +0530

    [Jaykant] Add Problem 9 Multiple of Number

---
+++ ProblemSet2/Problem9.cs
@@ -0,0 +1,20 @@
+﻿namespace ProblemSet2;
+
+public class Problem9
+{
+    public void Multiple(int x)
+    {
+        for(int i = x; i > 0; i--)
+        {
+            if (x % i == 0)
+            {
+                Console.WriteLine(i);
+
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
--- ProblemSet2/Program.cs
+++ ProblemSet2/Program.cs
@@ -5,91 +5,91 @@ class Program
     static void Main(string[] args)
     {
         // Problem 0 -> 0
-        //Console.WriteLine("Enter the Year: ");
-        //int year = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the Year: ");
+        int year = int.Parse(Console.ReadLine());
 
-        //Problem0 p0 = new Problem0();
-        //p0.LeapYear(year);
+        Problem0 p0 = new Problem0();
+        p0.LeapYear(year);
 
 
         // Problem 0 -> 1
-        //Console.WriteLine("Enter the marks of 3 subjects: ");
-        //int phy = int.Parse(Console.ReadLine());
-        //int chem = int.Parse(Console.ReadLine());
-        //int math = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the marks of 3 subjects: ");
+        int phy = int.Parse(Console.ReadLine());
+        int chem = int.Parse(Console.ReadLine());
+        int math = int.Parse(Console.ReadLine());
 
-        //Problem01 p01 = new Problem01();
-        //p01.Grade(phy, chem, math);
+        Problem01 p01 = new Problem01();
+        p01.Grade(phy, chem, math);
 
 
         // Problem 1
-        //Console.WriteLine("Enter number to check Prime: ");
-        //int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number to check Prime: ");
+        int num = Convert.ToInt32(Console.ReadLine());
 
-        //Problem1 p1 = new Problem1();
-        //p1.IsPrime(num);
+        Problem1 p1 = new Problem1();
+        p1.IsPrime(num);
 
 
 
         // Problem 2
-        //Console.WriteLine("Enter the number input: ");
-        //int inp = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number input: ");
+        int inp = int.Parse(Console.ReadLine());
 
-        //Problem2 p2 = new Problem2();
-        //p2.FizzBuzz(inp);
+        Problem2 p2 = new Problem2();
+        p2.FizzBuzz(inp);
 
 
 
         // Problem 3
-        //Console.WriteLine("Enter the number input: ");
-        //int inpu = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number input: ");
+        int inpu = int.Parse(Console.ReadLine());
 
-        //Problem3 p3 = new Problem3();
-        //p3.Fizz(inpu);
+        Problem3 p3 = new Problem3();
+        p3.Fizz(inpu);
 
 
 
         // Problem 4
-        //Console.WriteLine("Enter the Weight & Height");
-        //double weight = Convert.ToDouble(Console.ReadLine());
-        //double height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the Weight & Height");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        double height = Convert.ToDouble(Console.ReadLine());
 
-        //Problem4 p4 = new Problem4();
-        //p4.BMI(weight, height);
+        Problem4 p4 = new Problem4();
+        p4.BMI(weight, height);
 
 
 
 
         // Problem 5
-        //Console.WriteLine("Enter the Age & Height");
-        //int age1 = Convert.ToInt32(Console.ReadLine());
-        //int age2 = Convert.ToInt32(Console.ReadLine());
-        //int age3 = Convert.ToInt32(Console.ReadLine());
-        //double ht1 = Convert.ToDouble(Console.ReadLine());
-        //double ht2 = Convert.ToDouble(Console.ReadLine());
-        //double ht3 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the Age & Height");
+        int age1 = Convert.ToInt32(Console.ReadLine());
+        int age2 = Convert.ToInt32(Console.ReadLine());
+        int age3 = Convert.ToInt32(Console.ReadLine());
+        double ht1 = Convert.ToDouble(Console.ReadLine());
+        double ht2 = Convert.ToDouble(Console.ReadLine());
+        double ht3 = Convert.ToDouble(Console.ReadLine());
 
-        //Problem5 p5 = new Problem5();
-        //p5.AgeHeight(age1, age2,age3, ht1, ht2, ht3);
+        Problem5 p5 = new Problem5();
+        p5.AgeHeight(age1, age2, age3, ht1, ht2, ht3);
 
 
 
         // Problem 6
-        //Console.WriteLine("Enter the number ");
-        //int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number ");
+        int number = int.Parse(Console.ReadLine());
 
-        //Problem6 p6 = new Problem6();
-        //p6.Greater(number);
+        Problem6 p6 = new Problem6();
+        p6.Greater(number);
 
 
 
         // Problem 7
-        //Console.WriteLine("Enter the number and power: ");
-        //int numb = int.Parse(Console.ReadLine());
-        //int power = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number and power: ");
+        int numb = int.Parse(Console.ReadLine());
+        int power = int.Parse(Console.ReadLine());
 
-        //Problem7 p7 = new Problem7();
-        //p7.Power(numb,  power);
+        Problem7 p7 = new Problem7();
+        p7.Power(numb, power);
 
 
 
@@ -99,6 +99,16 @@ class Program
 
         Problem8 p8 = new Problem8();
         p8.GreatFactor(numbe);
+
+
+
+        // Problem 9
+        Console.WriteLine("Enter the number x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Problem9 p9 = new Problem9();
+        p9.Multiple(x);
+
 
 
         Console.ReadKey();
