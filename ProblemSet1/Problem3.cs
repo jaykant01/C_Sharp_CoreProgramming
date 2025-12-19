61c0e12cd11148f002264fa14deac0fb1bd066d7
jaykant01 <jaykanty24092004@gmail.com>
2025-12-19 11:31:46 +0530

    [Jaykant] Add Problem 3 Largest

---
+++ ProblemSet1/Problem3.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemSet1;
+
+public class Problem3
+{
+    public void Largest(int x, int y, int z)
+    {
+        if (x > y && x > z)
+        {
+            Console.WriteLine("Is the first number the largest? Yes");
+        }else if (y > x && y > z)
+        {
+            Console.WriteLine("Is the second number the largest? Yes");
+        }else
+        {
+            Console.WriteLine("Is the third number the largest? Yes");
+        }
+    }
+}
--- ProblemSet1/Program.cs
+++ ProblemSet1/Program.cs
@@ -13,18 +13,25 @@ class Program
 
 
         // Problem 2
-        Console.WriteLine("Enter number a, b & c: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        int b = Convert.ToInt32(Console.ReadLine());
-        int c = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter number a, b & c: ");
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //int c = Convert.ToInt32(Console.ReadLine());
 
-        Problem2 p2 = new Problem2();
-        p2.Smallest(a, b, c);
-
-
+        //Problem2 p2 = new Problem2();
+        //p2.Smallest(a, b, c);
 
 
-        
+        // Problem 3
+        Console.WriteLine("Enter number x, y & z: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        int z = Convert.ToInt32(Console.ReadLine());
+
+        Problem3 p3 = new Problem3();
+        p3.Largest(x, y, z);
+
+
 
         Console.ReadKey();
     }
