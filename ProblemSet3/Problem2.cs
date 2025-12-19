9bd9029f404c52f579ea2e9cc976e05d13668647
jaykant01 <jaykanty24092004@gmail.com>
2025-12-20 01:06:00 +0530

    [Jaykant] Add Problem 2 Count Digits

---
+++ ProblemSet3/Problem2.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemSet3;
+
+public class Problem2
+{
+    public void Counter(int num)
+    {
+        int count = 0;
+
+        while (num > 0)
+        {
+            int rem = num % 10;
+            count += 1;
+            num /= 10;
+        }
+
+        Console.WriteLine(count);
+    }
+}
--- ProblemSet3/Program.cs
+++ ProblemSet3/Program.cs
@@ -5,11 +5,21 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        Console.WriteLine("Enter the number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter the number: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
 
-        Problem1 p1 = new Problem1();
-        p1.Armstrong(number);
+        //Problem1 p1 = new Problem1();
+        //p1.Armstrong(number);
+
+
+        // Problem 2
+        Console.WriteLine("Enter the Num: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Problem2 p2 = new Problem2();
+        p2.Counter(num);
+
+
 
         Console.ReadKey();
     }
