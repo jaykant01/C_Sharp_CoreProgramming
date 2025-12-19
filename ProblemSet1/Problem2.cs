1f71c969c35225dd72389977d4055d431e8aa10d
jaykant01 <jaykanty24092004@gmail.com>
2025-12-19 11:20:43 +0530

    [Jaykant] Add Problem 2 Smallest

---
+++ ProblemSet1/Problem2.cs
@@ -0,0 +1,16 @@
+﻿namespace ProblemSet1;
+
+public class Problem2
+{
+    public void Smallest(int a,  int b,  int c)
+    {
+        if (a < b && a < c)
+        {
+            Console.WriteLine("Is the first number the smallest? Yes");
+        }
+        else
+        {
+            Console.WriteLine("Is the first number the smallest? No");
+        }
+    }
+}
--- ProblemSet1/Program.cs
+++ ProblemSet1/Program.cs
@@ -1,22 +1,32 @@
 ﻿namespace ProblemSet1;
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Problem 1
-            Console.Write("Enter a number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+        // Problem 1
+        //Console.Write("Enter a number: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
 
-            Problem1 c1 = new Problem1();
-            c1.CheckDivisible(number);
-
-            
-            
+        //Problem1 c1 = new Problem1();
+        //c1.CheckDivisible(number);
 
 
+        // Problem 2
+        Console.WriteLine("Enter number a, b & c: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
-        }
+        Problem2 p2 = new Problem2();
+        p2.Smallest(a, b, c);
+
+
+
+
+        
+
+        Console.ReadKey();
     }
+}
 
