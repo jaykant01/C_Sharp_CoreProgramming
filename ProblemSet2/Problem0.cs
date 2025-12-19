600aeaf744d349f16ff7069daead5686e64a75f8
jaykant01 <jaykanty24092004@gmail.com>
2025-12-19 15:41:24 +0530

    [Jaykant] Add Problem 0 Leap Year

---
+++ ProblemSet2/Problem0.cs
@@ -0,0 +1,15 @@
+﻿namespace ProblemSet2;
+
+public class Problem0
+{
+    public void LeapYear(int year)
+    {
+        if ((year % 4 == 0) || (year % 400 == 0 && year % 100 != 0))
+        {
+            Console.WriteLine("It is a Leap Year " + year);
+        }else
+        {
+            Console.WriteLine("The Year is not a Leap Year " + year);
+        }
+    }
+}
---
+++ ProblemSet2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ProblemSet2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 0 -> 0
+        Console.WriteLine("Enter the Year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        Problem0 p0 = new Problem0();
+        p0.LeapYear(year);
+
+
+        Console.ReadKey();
+    }
+}
