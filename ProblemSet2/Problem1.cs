9f034d61e325d3a3d1a46e739b0147c6df38e807
jaykant01 <jaykanty24092004@gmail.com>
2025-12-19 16:21:03 +0530

    [Jaykant] Add Problem 1 IsPrime

---
+++ ProblemSet2/Problem1.cs
@@ -0,0 +1,22 @@
+﻿namespace ProblemSet2;
+
+public class Problem1
+{
+     public void IsPrime(int num)
+    {
+        bool flag = true;
+        for (int i=2; i<num; i++)
+        {
+            if (num % i == 0)
+            {
+                flag = false;
+                break;
+            }else
+            {
+                flag = true;
+            }
+        }
+
+        Console.WriteLine(flag);
+    }
+}
--- ProblemSet2/Program.cs
+++ ProblemSet2/Program.cs
@@ -13,14 +13,21 @@ class Program
 
 
         // Problem 0 -> 1
-        Console.WriteLine("Enter the marks of 3 subjects: ");
-        int phy = int.Parse(Console.ReadLine());
-        int chem = int.Parse(Console.ReadLine());
-        int math = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the marks of 3 subjects: ");
+        //int phy = int.Parse(Console.ReadLine());
+        //int chem = int.Parse(Console.ReadLine());
+        //int math = int.Parse(Console.ReadLine());
 
-        Problem01 p01 = new Problem01();
-        p01.Grade(phy, chem, math);
+        //Problem01 p01 = new Problem01();
+        //p01.Grade(phy, chem, math);
 
+
+        // Problem 1
+        Console.WriteLine("Enter number to check Prime: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Problem1 p1 = new Problem1();
+        p1.IsPrime(num);
 
 
 
