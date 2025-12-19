6828e5871d7e26cc2109a56af3ba54a6d02cee68
jaykant01 <jaykanty24092004@gmail.com>
2025-12-20 00:49:41 +0530

    [Jaykant] Add Problem 1 Armstrong Number

---
+++ ProblemSet3/Problem1.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemSet3;
+
+public class Problem1
+{
+    public void Armstrong(int number)
+    {
+        int sum = 0;
+        int ogNumber = number;
+
+        while (ogNumber > 0)
+        {
+            int rem = ogNumber % 10;
+            sum += (rem * rem * rem);
+            ogNumber /= 10;
+        }
+
+        Console.WriteLine("Sum is: " + sum);
+
+        if (number == sum)
+        {
+            Console.WriteLine("Armstrong number");
+        }else
+        {
+            Console.WriteLine("Not an Armstrong Number");
+        }
+
+    }
+}
---
+++ ProblemSet3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProblemSet3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        Console.WriteLine("Enter the number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Problem1 p1 = new Problem1();
+        p1.Armstrong(number);
+
+        Console.ReadKey();
+    }
+}
