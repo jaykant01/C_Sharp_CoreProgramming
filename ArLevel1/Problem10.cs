5fd31cd49ffbd0b5e456a50c6c03e631eedd5b01
jaykant01 <jaykanty24092004@gmail.com>
2025-12-20 15:39:00 +0530

    [Jaykant] Add .Problem 10 FizzBuzz

---
+++ ArLevel1/Problem10.cs
@@ -0,0 +1,39 @@
+﻿namespace ArLevel1;
+
+public class Problem10
+{
+    public void FizzBuzz()
+    {
+        int num = int.Parse(Console.ReadLine());
+        string[] arr = new string[num+1];
+
+        if (num <= 0)
+        {
+            Console.WriteLine("Please enter a positive integer.");
+            return;
+        }
+            for(int i = 1; i <= num; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    arr[i] = "FizzBuzz";
+                }else if ( i % 5 == 0)
+                {
+                    arr[i] = "Buzz";
+                }else if (i % 3 == 0)
+                {
+                    arr[i] = "Fizz";
+                }
+                else
+                {
+                arr[i] = i.ToString();
+                }
+            }
+        
+
+        for(int i=1; i <= num; i++)
+        {
+            Console.WriteLine($"Postion {i} = {arr[i]}");
+        }
+    }
+}
--- ArLevel1/Program.cs
+++ ArLevel1/Program.cs
@@ -5,60 +5,60 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        //Console.WriteLine("Enter the size of an Array: ");
-        //int n = Convert.ToInt32(Console.ReadLine());
-        //Console.WriteLine($"Enter the age for {n} persons: ");
-        //Problem1 p1 = new Problem1();
-        //p1.Age(n);
+        Console.WriteLine("Enter the size of an Array: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Enter the age for {n} persons: ");
+        Problem1 p1 = new Problem1();
+        p1.Age(n);
 
 
 
         // Problem 2
-        //Console.WriteLine("Enter size of array and arrays");
-        //Problem2 p2 = new Problem2();
-        //p2.Compare();
+        Console.WriteLine("Enter size of array and arrays");
+        Problem2 p2 = new Problem2();
+        p2.Compare();
 
 
 
         // Problem 3
-        //Console.WriteLine("Enter Table to Print & Enter range: ");
-        //Problem3 p3 = new Problem3();
-        //p3.Table();
+        Console.WriteLine("Enter Table to Print & Enter range: ");
+        Problem3 p3 = new Problem3();
+        p3.Table();
 
 
 
         // Problem 4
-        //Console.WriteLine("Enter the array elem: ");
-        //Problem4 p4 = new Problem4();
-        //p4.Stop();
+        Console.WriteLine("Enter the array elem: ");
+        Problem4 p4 = new Problem4();
+        p4.Stop();
 
 
 
         // Problem 5
-        //Console.WriteLine("Enter Table no to print: ");
-        //Problem5 p5 = new Problem5();
-        //p5.Multiple();
+        Console.WriteLine("Enter Table no to print: ");
+        Problem5 p5 = new Problem5();
+        p5.Multiple();
 
 
 
         // Problem 6
-        //Console.WriteLine("Enter the height of players in array: ");
-        //Problem6 p6 = new Problem6();
-        //p6.Footbal();
+        Console.WriteLine("Enter the height of players in array: ");
+        Problem6 p6 = new Problem6();
+        p6.Footbal();
 
 
 
         // Problem 7
-        //Console.WriteLine("Enter the number ");
-        //Problem7 p7 = new Problem7();
-        //p7.EvenOdd();
+        Console.WriteLine("Enter the number ");
+        Problem7 p7 = new Problem7();
+        p7.EvenOdd();
 
 
 
         // Problem 8
-        //Console.WriteLine("Enter the number: ");
-        //Problem8 p8 = new Problem8();
-        //p8.Factors();
+        Console.WriteLine("Enter the number: ");
+        Problem8 p8 = new Problem8();
+        p8.Factors();
 
 
 
@@ -67,6 +67,13 @@ class Program
         Console.WriteLine("Enter the  number: ");
         Problem9 p9 = new Problem9();
         p9.TwoDArray();
+
+
+
+        // Problem 10
+        Console.WriteLine("Enter the number: ");
+        Problem10 p10 = new Problem10();
+        p10.FizzBuzz();
 
 
 
