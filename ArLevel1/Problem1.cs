de039d460354a1f5c0ee53a95597c793bfa2458c
jaykant01 <jaykanty24092004@gmail.com>
2025-12-20 10:58:26 +0530

    [Jaykant] Add .Problem 1 Voting with Array

---
+++ ArLevel1/Problem1.cs
@@ -0,0 +1,27 @@
+﻿namespace ArLevel1;
+
+public class Problem1
+{
+    public void Age(int n)
+    {
+        
+        int[] age = new int[n];
+
+        for (int i=0; i<n; i++)
+        {
+            age[i] = Convert.ToInt32(Console.ReadLine());
+
+            if (age[i] >= 18)
+            {
+                Console.WriteLine($"The student with the age {age[i]} can vote");
+                
+            }else
+            {
+                Console.WriteLine($"The student with the age {age[i]} cannot vote");
+            }
+        }
+        // To Print the Array of Age
+        string result = $"[{string.Join(", ", age)}]";
+        Console.WriteLine(result);
+    }
+}
---
+++ ArLevel1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ArLevel1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        Console.WriteLine("Enter the size of an Array: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Enter the age for {n} persons: ");
+        Problem1 p1 = new Problem1();
+        p1.Age(n);
+
+        Console.ReadKey();
+    }
+}
