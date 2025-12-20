c82b2f5adadada6b6ada54d6660164eab7ad8470
jaykant01 <jaykanty24092004@gmail.com>
2025-12-20 11:23:56 +0530

    [Jaykant] Add .Problem 2 Compare

---
+++ ArLevel1/Problem2.cs
@@ -0,0 +1,43 @@
+﻿namespace ArLevel1;
+
+public class Problem2
+{
+    public void Compare()
+    {
+        int s = int.Parse(Console.ReadLine());
+        int[] arr = new int[s];
+
+        for (int i=0; i<s; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+            
+            // Check Positive or Negative
+            if (arr[i] >= 0)
+            {
+                string eveOdd = (arr[i] % 2 == 0) ? "Even" : "Odd";
+                Console.WriteLine("The number is Positive and " + eveOdd);
+            }
+            else
+            {
+                Console.WriteLine($"The number {arr[i]} is negative");
+            }
+        }
+
+        string printArry = $"[{string.Join(", ", arr)}]";
+        Console.WriteLine( printArry );
+
+        if (arr[0] == arr[s - 1])
+        {
+            Console.WriteLine("first and last element are Equal");
+        }else if (arr[0] > arr[s - 1])
+        {
+            Console.WriteLine("Array first element is greater");
+        }
+        else
+        {
+            Console.WriteLine("Array last element is greater");
+        }
+
+
+    }
+}
--- ArLevel1/Program.cs
+++ ArLevel1/Program.cs
@@ -5,11 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        Console.WriteLine("Enter the size of an Array: ");
-        int n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Enter the age for {n} persons: ");
-        Problem1 p1 = new Problem1();
-        p1.Age(n);
+        //Console.WriteLine("Enter the size of an Array: ");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine($"Enter the age for {n} persons: ");
+        //Problem1 p1 = new Problem1();
+        //p1.Age(n);
+
+
+
+        // Problem 2
+        Console.WriteLine("Enter size of array and arrays");
+        Problem2 p2 = new Problem2();
+        p2.Compare();
 
         Console.ReadKey();
     }
