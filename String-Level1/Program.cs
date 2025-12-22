597cc84958551a480e230e9c4050d8f6702e8af6
jaykant01 <jaykanty24092004@gmail.com>
2025-12-22 12:08:16 +0530

    [Jaykant] Add. Problem 2 Sub Strings

--- String-Level1/Program.cs
+++ String-Level1/Program.cs
@@ -5,19 +5,27 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1 String Compare
-        Console.WriteLine("Enter Two Strings: ");
-        string s = Console.ReadLine();
-        string k = Console.ReadLine();
+        //Console.WriteLine("Enter Two Strings: ");
+        //string s = Console.ReadLine();
+        //string k = Console.ReadLine();
 
         // Comparing by Inbuilt method
-        CompareTwoStrings cp = new CompareTwoStrings();
-        cp.Compare(s,k);
+        //CompareTwoStrings cp = new CompareTwoStrings();
+        //cp.Compare(s,k);
 
         // Comparing by CharAt method
-        CompareTwoStrings sc = new CompareTwoStrings();
-        bool res1 = sc.CompareUsingCharAt(s,k);
-        Console.WriteLine("\nManual Comparison Result: " + res1);
+        //CompareTwoStrings sc = new CompareTwoStrings();
+        //bool res1 = sc.CompareUsingCharAt(s,k);
+        //Console.WriteLine("\nManual Comparison Result: " + res1);
 
+
+
+
+        // Problem 2 Sub String
+        Console.WriteLine("Enter string input");
+        
+        SubString sa = new SubString();
+        sa.CompareUsingCharAt();
 
         Console.ReadKey();
     }
---
+++ String-Level1/SubString.cs
@@ -0,0 +1,29 @@
+﻿namespace String_Level1;
+
+public class SubString
+{
+    public void CompareUsingCharAt()
+    {
+        string input = Console.ReadLine();
+        int st = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        string sub = "";
+
+        for (int i=st; i<end; i++)
+        {
+            sub += input[(i)];
+        }
+
+        Console.WriteLine(sub);
+
+        // Substring using inbuilt method
+        string insub = input.Substring(3,3);
+        Console.WriteLine(insub);
+
+        // Substring using inbuilt method from start index to end index
+        string insub2 = input.Substring(2);
+        Console.WriteLine(insub2);
+        
+    }
+
+}
