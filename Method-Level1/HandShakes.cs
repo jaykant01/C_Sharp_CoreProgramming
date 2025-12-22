8c83a540a3feb729078a494cdea941503dd2004b
jaykant01 <jaykanty24092004@gmail.com>
2025-12-23 02:45:54 +0530

    [Jaykant] Add .Problem 2 HandShakes

---
+++ Method-Level1/HandShakes.cs
@@ -0,0 +1,10 @@
+﻿namespace Method_Level1;
+
+public class HandShakes
+{
+    public int CalculateHandshakes(int n)
+    {
+        int handshakes = (n * (n - 1)) / 2;
+        return handshakes;
+    }
+}
--- Method-Level1/Program.cs
+++ Method-Level1/Program.cs
@@ -5,13 +5,25 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1 calculate Simple Interest
-        Console.WriteLine("Enter Principal, Rate, time: ");
-        double principal = Convert.ToDouble(Console.ReadLine());
-        double rate = Convert.ToDouble(Console.ReadLine());
-        double time = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("Enter Principal, Rate, time: ");
+        //double principal = Convert.ToDouble(Console.ReadLine());
+        //double rate = Convert.ToDouble(Console.ReadLine());
+        //double time = Convert.ToDouble(Console.ReadLine());
 
-        SimpleInterest obj = new SimpleInterest();
-        Console.WriteLine("SI is: " + obj.CalculateSI(principal, rate, time));
+        //SimpleInterest obj = new SimpleInterest();
+        //Console.WriteLine("SI is: " + obj.CalculateSI(principal, rate, time));
+
+
+
+        // Problem 2 
+        Console.WriteLine("Enter number of students:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        HandShakes obj = new HandShakes();
+        int result = obj.CalculateHandshakes(n);
+
+        Console.WriteLine("Maximum number of handshakes = " + result);
+
+
 
         Console.ReadKey();
     }
