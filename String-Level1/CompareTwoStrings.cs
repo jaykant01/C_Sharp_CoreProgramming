7b676fe1ce69949d46de85e146eff548a308e901
jaykant01 <jaykanty24092004@gmail.com>
2025-12-22 11:31:05 +0530

    [Jaykant] Add. Problem 1 Compare Two Strings

---
+++ String-Level1/CompareTwoStrings.cs
@@ -0,0 +1,35 @@
+﻿namespace String_Level1;
+
+public class CompareTwoStrings
+{/*Compare Two Strings Using charAt()
+  */
+    public void Compare(string s, string k)
+    {
+        // In built method compare
+        bool res2 = s.Equals(k);
+        bool res3 = string.Equals(s, k);
+
+        Console.WriteLine("Built-in Equals Result1: " + res2);
+        Console.WriteLine("Built-in Equals Result2: " + res3);
+         
+    }
+
+    // compare using charAt
+    public bool CompareUsingCharAt(string s, string k)
+    {
+        if (s.Length != k.Length)
+        {
+            return false;
+        }
+
+        for(int i=0; i<s.Length; i++)
+        {
+            if (s[i] != k[i])
+            {
+                return false;
+            }
+             
+        }
+        return true;
+    }
+}
---
+++ String-Level1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace String_Level1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1 String Compare
+        Console.WriteLine("Enter Two Strings: ");
+        string s = Console.ReadLine();
+        string k = Console.ReadLine();
+
+        // Comparing by Inbuilt method
+        CompareTwoStrings cp = new CompareTwoStrings();
+        cp.Compare(s,k);
+
+        // Comparing by CharAt method
+        CompareTwoStrings sc = new CompareTwoStrings();
+        bool res1 = sc.CompareUsingCharAt(s,k);
+        Console.WriteLine("\nManual Comparison Result: " + res1);
+
+
+        Console.ReadKey();
+    }
+}
