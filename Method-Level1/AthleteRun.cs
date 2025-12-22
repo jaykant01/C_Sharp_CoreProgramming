6b6c36ecb08b9d2782f8c5791177a8d58373d952
jaykant01 <jaykanty24092004@gmail.com>
2025-12-23 02:50:11 +0530

    [Jaykant] Add .Problem 4 Trinagular Run

---
+++ Method-Level1/AthleteRun.cs
@@ -0,0 +1,11 @@
+﻿namespace Method_Level1;
+
+public class AthleteRun
+{
+    public double CalculateRounds(double side1, double side2, double side3)
+    {
+        double perimeter = side1 + side2 + side3;
+        double rounds = 5000 / perimeter;
+        return rounds;
+    }
+}
--- Method-Level1/Program.cs
+++ Method-Level1/Program.cs
@@ -15,15 +15,26 @@ class Program
 
 
 
-        // Problem 2 
-        Console.WriteLine("Enter number of students:");
-        int n = Convert.ToInt32(Console.ReadLine());
-        HandShakes obj = new HandShakes();
-        int result = obj.CalculateHandshakes(n);
+        // Problem 2 hand Shakes
+        //Console.WriteLine("Enter number of students:");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //HandShakes obj = new HandShakes();
+        //int result = obj.CalculateHandshakes(n);
 
-        Console.WriteLine("Maximum number of handshakes = " + result);
+        //Console.WriteLine("Maximum number of handshakes = " + result);
 
 
+
+        // Problem 4 Athlete runs
+        Console.WriteLine("Enter side 1, side 2, side 3 (in meters):");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+        double side2 = Convert.ToDouble(Console.ReadLine());
+        double side3 = Convert.ToDouble(Console.ReadLine());
+
+        AthleteRun obj = new AthleteRun();
+        double result = obj.CalculateRounds(side1, side2, side3);
+
+        Console.WriteLine("Number of rounds to complete 5 km run = " + result);
 
         Console.ReadKey();
     }
