b72f056427c00fdf3035987335e6f204fc3cba76
jaykant01 <jaykanty24092004@gmail.com>
2025-12-23 01:18:16 +0530

    [Jaykant] Add. Problem 7 Toggle Case

---
+++ String-Level2/ToggleCase.cs
@@ -0,0 +1,31 @@
+﻿namespace String_Level2;
+
+public class ToggleCase
+{
+    public void Toggle()
+    {
+        string s = Console.ReadLine();
+        string c = "";
+
+        for(int i=0; i<s.Length; i++)
+        {
+            char ch = s[i];
+            if(ch == ' ')
+            {
+                c += ch;
+            }
+            if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = (char)(ch + 32);
+                c += ch;
+            }
+            else
+            {
+                ch = (char)(ch - 32);
+                c += ch;
+            }
+        }
+
+        Console.WriteLine(c);
+    }
+}
