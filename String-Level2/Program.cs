6b78d3f3bbe9aabdd186b5a188314ba6cc82bbf4
jaykant01 <jaykanty24092004@gmail.com>
2025-12-22 16:01:48 +0530

    [Jaykant] Add. Problem 2 Reverse String

--- String-Level2/Program.cs
+++ String-Level2/Program.cs
@@ -5,8 +5,13 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1 Count Vowels and Consonants
-        VowelsAndConsonants vc = new VowelsAndConsonants();
-        vc.CountVowCon();
+        //VowelsAndConsonants vc = new VowelsAndConsonants();
+        //vc.CountVowCon();
+
+
+        // Problem 2 Reverse a String
+        ReverseString rv = new ReverseString();
+        rv.Reverse();
 
         Console.ReadKey();
     }
---
+++ String-Level2/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿namespace String_Level2;
+
+public class ReverseString
+{
+    public void Reverse()
+    {
+        string s = Console.ReadLine();
+        string r = "";
+        int st = 0;
+        int end = s.Length - 1;
+
+        for(int i=s.Length-1; i>=0; i--)
+        {
+            r += s[i];
+        }
+        Console.WriteLine(r);
+    }
+}
