1a75c372d869f47f772511e3b6c3643932c15ded
jaykant01 <jaykanty24092004@gmail.com>
2025-12-23 11:31:23 +0530

    [Jaykant] Add .Problem 2 Natural number using Recursion

---
+++ Method-Level2/NaturalNoRecursive.cs
@@ -0,0 +1,21 @@
+﻿namespace Method_Level2;
+
+public class NaturalNoRecursive
+{
+    public int Formula(int n)
+    {
+        int natural = n * (n + 1) / 2;
+        return natural;
+    }
+
+    public int Recursion(int n)
+    {
+        if(n == 0)
+        {
+            return n;
+        }
+        int sum = 0;
+        sum += n;
+        return sum;
+    }
+}
--- Method-Level2/Program.cs
+++ Method-Level2/Program.cs
@@ -5,27 +5,42 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1 Factors
-        Console.WriteLine("Enter a number:");
-        int number = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter a number:");
+        //int number = Convert.ToInt32(Console.ReadLine());
 
-        int[] factors = FindFactors.Factors(number);
-        // Print Factors
-        Console.WriteLine("Factors are:");
-        for (int i = 0; i < factors.Length; i++)
+        //int[] factors = FindFactors.Factors(number);
+        //// Print Factors
+        //Console.WriteLine("Factors are:");
+        //for (int i = 0; i < factors.Length; i++)
+        //{
+        //    Console.Write(factors[i] + " ");
+        //}
+        //Console.WriteLine();
+
+        //int sum = FindFactors.FindSum(factors);
+        //long product = FindFactors.FindProduct(factors);
+        //double sumOfSquares = FindFactors.FindSumOfSquares(factors);
+
+        //Console.WriteLine($"Sum of factors = {sum}");
+        //Console.WriteLine($"Product of factors = {product}");
+        //Console.WriteLine($"Sum of squares of factors = {sumOfSquares}");
+
+
+
+        // Problem 2 Natural number using Recursion
+        Console.WriteLine("Enter number n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        NaturalNoRecursive nat = new NaturalNoRecursive();
+        int res1 = nat.Formula(n);
+        int res2 = nat.Recursion(n);
+        Console.WriteLine("Natural no From Formula " +  res1);
+        Console.WriteLine("Natural no From Recursion " +  res1);
+
+        if(res1 == res2)
         {
-            Console.Write(factors[i] + " ");
+            Console.WriteLine("Both are Equal");
         }
-        Console.WriteLine();
-
-        int sum = FindFactors.FindSum(factors);
-        long product = FindFactors.FindProduct(factors);
-        double sumOfSquares = FindFactors.FindSumOfSquares(factors);
-
-        Console.WriteLine($"Sum of factors = {sum}");
-        Console.WriteLine($"Product of factors = {product}");
-        Console.WriteLine($"Sum of squares of factors = {sumOfSquares}");
-
-
 
         Console.ReadKey();
     }
