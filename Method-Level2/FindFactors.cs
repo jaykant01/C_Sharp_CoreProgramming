12cddfcd1c95e7d3a51551cf079f794d085b06fc
jaykant01 <jaykanty24092004@gmail.com>
2025-12-23 11:04:23 +0530

    [Jaykant] Add .Find Factors

---
+++ Method-Level2/FindFactors.cs
@@ -0,0 +1,66 @@
+﻿namespace Method_Level2;
+
+public class FindFactors
+{
+    public static int[] Factors(int number)
+    {
+        int count = 0;
+
+        for(int i=1;  i<number; i++)
+        {
+            if (number % i == 0)
+            {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        for(int i=1;  i<number; i++)
+        {
+            if (number % i == 0)
+            {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+        
+    }
+
+
+    // Find Sum of factors
+    public static int FindSum(int[] factors)
+    {
+        int sum = 0;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            sum += factors[i];
+        }
+        return sum;
+    }
+
+
+    // Method to find product
+    public static long FindProduct(int[] factors)
+    {
+        long product = 1;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            product *= factors[i];
+        }
+        return product;
+    }
+
+
+    // method to find Sum of Square factors
+    public static double FindSumOfSquares(int[] factors)
+    {
+        double sumOfSquares = 0;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            sumOfSquares += Math.Pow(factors[i], 2);
+        }
+        return sumOfSquares;
+    }
+}
---
+++ Method-Level2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Method_Level2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1 Factors
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int[] factors = FindFactors.Factors(number);
+        // Print Factors
+        Console.WriteLine("Factors are:");
+        for (int i = 0; i < factors.Length; i++)
+        {
+            Console.Write(factors[i] + " ");
+        }
+        Console.WriteLine();
+
+        int sum = FindFactors.FindSum(factors);
+        long product = FindFactors.FindProduct(factors);
+        double sumOfSquares = FindFactors.FindSumOfSquares(factors);
+
+        Console.WriteLine($"Sum of factors = {sum}");
+        Console.WriteLine($"Product of factors = {product}");
+        Console.WriteLine($"Sum of squares of factors = {sumOfSquares}");
+
+
+
+        Console.ReadKey();
+    }
+}
