47a1685730e1029ba8222102788af52af982275e
jaykant01 <jaykanty24092004@gmail.com>
2025-12-24 17:03:46 +0530

    [Jaykant] Add .find the shortest, tallest, and mean height of players

---
+++ Method-Level3/FootBallTeam.cs
@@ -0,0 +1,58 @@
+﻿namespace Method_Level3;
+/* Create a program to find the shortest, tallest, and mean height of players present in a
+football team.
+ */
+public class FootBallTeam
+{
+    public int SumElements(int[] height)
+    {
+        int sum = 0;
+        for(int i=0; i<height.Length; i++)
+        {
+            sum += height[i];
+        }
+
+        return sum;
+    }
+
+
+    public double MeanElements(int[] height)
+    {
+        double mean = SumElements(height) / height.Length;
+
+        Console.WriteLine("Mean of elements is " + mean);
+        return mean;
+    }
+
+
+     public int SortestHeight(int[] height)
+    {
+        int shortest = int.MaxValue;
+        for(int i=0; i<height.Length; i++)
+        {
+            if(shortest > height[i])
+            {
+                shortest = height[i];
+            }
+        }
+        Console.WriteLine("Shortest elements is " + shortest);
+        return shortest;
+    }
+
+
+    public int TallestHeight(int[] height)
+    {
+        int tall = int.MinValue;
+        for (int i = 0; i < height.Length; i++)
+        {
+            if (tall < height[i])
+            {
+                tall = height[i];
+            }
+        }
+        Console.WriteLine("Tallest elements is " + tall);
+        return tall;
+    }
+
+
+}
--- Method-Level3/Program.cs
+++ Method-Level3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Problem 1 
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] height = new int[n];
+
+        for(int i=0; i<n; i++)
+        {
+            height[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        FootBallTeam fb = new FootBallTeam();
+        int sum = fb.SumElements(height);
+        Console.WriteLine("Sum of all elements is " + sum);
+        fb.MeanElements(height);
+        fb.SortestHeight(height);
+        fb.TallestHeight(height);
+
+
+        Console.ReadKey();
     }
 }
