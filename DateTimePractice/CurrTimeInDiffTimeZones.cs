f20528d5970e68859ad0c64ae1439b9b9c5dc0eb
jaykant01 <jaykanty24092004@gmail.com>
2025-12-26 14:32:31 +0530

    [Jaykant] Add .Time Zones and DateTimeOffset

---
+++ DateTimePractice/CurrTimeInDiffTimeZones.cs
@@ -0,0 +1,24 @@
+﻿namespace DateTimePractice;
+/* Write a program that displays the current time in different time zones:
+ */
+public class CurrTimeInDiffTimeZones
+{
+    public void DisplayTimeZones()
+    {
+        DateTimeOffset utcTime = DateTimeOffset.UtcNow;
+
+        TimeZoneInfo gmtZone = TimeZoneInfo.Utc;
+        TimeZoneInfo istZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+        TimeZoneInfo pstZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+        DateTimeOffset gmtTime = TimeZoneInfo.ConvertTime(utcTime, gmtZone);
+        DateTimeOffset istTime = TimeZoneInfo.ConvertTime(utcTime, istZone);
+        DateTimeOffset pstTime = TimeZoneInfo.ConvertTime(utcTime, pstZone);
+
+        Console.WriteLine("Current Time in Different Time Zones");
+        Console.WriteLine("GMT : " + gmtTime);
+        Console.WriteLine("IST : " + istTime);
+        Console.WriteLine("PST : " + pstTime);
+    }
+    
+}
---
+++ DateTimePractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace DateTimePractice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Problem 1
+        CurrTimeInDiffTimeZones tz = new CurrTimeInDiffTimeZones();
+        tz.DisplayTimeZones();
+
+        //Console.WriteLine("Enter Date in this format dd-MM-yyyy ");
+        //string inputDate = Console.ReadLine();
+        //DateTime date = DateTime.ParseExact(inputDate);
+        //DateTime utc = 
+
+        Console.ReadKey();
+    }
+}
