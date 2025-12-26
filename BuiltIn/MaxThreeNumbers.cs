5f829926cceb7d1d3a6f19ed23d0e1ed066f864c
jaykant01 <jaykanty24092004@gmail.com>
2025-12-26 11:01:20 +0530

    [Jaykant] Add .Maximum of Three Number

---
+++ BuiltIn/MaxThreeNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace BuiltIn;
+/* Write a program that takes three integer inputs from the user and finds the maximum of the
+three numbers.
+ */
+public class MaxThreeNumbers
+{
+    public void FindMaximum()
+    {
+        int[] numbers = TakeInput();
+        int max = CalculateMaximum(numbers);
+
+        Console.WriteLine("\nMaximum number is: " + max);
+    }
+
+    int[] TakeInput()
+    {
+        int[] nums = new int[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Enter number " + (i + 1) + ": ");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        return nums;
+    }
+
+    int CalculateMaximum(int[] nums)
+    {
+        int max = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > max)
+            {
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+}
--- BuiltIn/Program.cs
+++ BuiltIn/Program.cs
@@ -5,11 +5,19 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        Console.WriteLine("Think of a number between 1 and 100.");
-        Console.WriteLine("Give feedback:");
-        Console.WriteLine("H = Too High, L = Too Low, C = Correct\n");
+        //Console.WriteLine("Think of a number between 1 and 100.");
+        //Console.WriteLine("Give feedback:");
+        //Console.WriteLine("H = Too High, L = Too Low, C = Correct\n");
 
-        GuessingGame game = new GuessingGame();
-        game.PlayGame();
+        //GuessingGame game = new GuessingGame();
+        //game.PlayGame();
+
+
+
+        // Problem 2
+        MaxThreeNumbers mx = new MaxThreeNumbers();
+        mx.FindMaximum();
+
+        Console.ReadKey();
     }
 }
