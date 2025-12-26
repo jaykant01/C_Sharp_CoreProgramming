c528d0444900f4b3672c0aab0ef14d0953484e54
jaykant01 <jaykanty24092004@gmail.com>
2025-12-26 10:53:15 +0530

    [Jaykant] Add .Number Guessing Game

---
+++ BuiltIn/GuessingGame.cs
@@ -0,0 +1,55 @@
+﻿namespace BuiltIn;
+
+public class GuessingGame
+{
+    int low = 1;
+    int high = 100;
+    bool isCorrect = false;
+
+    public void PlayGame()
+    {
+        while (!isCorrect)
+        {
+            int guess = GenerateGuess();
+            Console.WriteLine($"Computer guesses: {guess}");
+
+            char feedback = GetUserFeedback();
+            UpdateRange(feedback, guess);
+        }
+
+        Console.WriteLine("Computer guessed your number correctly!");
+    }
+
+    int GenerateGuess()
+    {
+        return (low + high) / 2;
+    }
+
+    char GetUserFeedback()
+    {
+        Console.Write("Enter feedback (H/L/C): ");
+        return Char.ToUpper(Console.ReadKey().KeyChar);
+    }
+
+    void UpdateRange(char feedback, int guess)
+    {
+        Console.WriteLine();
+
+        if (feedback == 'H')
+        {
+            high = guess - 1;
+        }
+        else if (feedback == 'L')
+        {
+            low = guess + 1;
+        }
+        else if (feedback == 'C')
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter H, L, or C.");
+        }
+    }
+}
---
+++ BuiltIn/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BuiltIn;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        Console.WriteLine("Think of a number between 1 and 100.");
+        Console.WriteLine("Give feedback:");
+        Console.WriteLine("H = Too High, L = Too Low, C = Correct\n");
+
+        GuessingGame game = new GuessingGame();
+        game.PlayGame();
+    }
+}
