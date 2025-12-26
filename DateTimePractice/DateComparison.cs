901179001cdb570601557ec76aaad52cf934afbb
jaykant01 <jaykanty24092004@gmail.com>
2025-12-26 17:03:02 +0530

    [Jaykant] Add .Date Comparison

---
+++ DateTimePractice/DateComparison.cs
@@ -0,0 +1,25 @@
+﻿namespace DateTimePractice;
+/* Write a program that:
+● Takes two date inputs and compares them to check if the first date is
+before, after, or the same as the second date.
+ */
+public class DateComparison
+{
+    public void CompareDates(DateTime date1, DateTime date2)
+    {
+        int result = DateTime.Compare(date1, date2);
+
+        if (result < 0)
+        {
+            Console.WriteLine("First date is BEFORE the second date.");
+        }
+        else if (result > 0)
+        {
+            Console.WriteLine("First date is AFTER the second date.");
+        }
+        else
+        {
+            Console.WriteLine("Both dates are the SAME.");
+        }
+    }
+}
--- DateTimePractice/Program.cs
+++ DateTimePractice/Program.cs
@@ -20,8 +20,19 @@ class Program
 
 
         // Problem 3
-        DateFormatter df = new DateFormatter();
-        df.DisplayFormats();
+        //DateFormatter df = new DateFormatter();
+        //df.DisplayFormats();
+
+
+
+        // Problem 4
+        Console.Write("Enter first date (yyyy-MM-dd): ");
+        DateTime date1 = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Enter second date (yyyy-MM-dd): ");
+        DateTime date2 = DateTime.Parse(Console.ReadLine());
+        DateComparison dc = new DateComparison();
+        dc.CompareDates(date1, date2);
 
         Console.ReadKey();
     }
