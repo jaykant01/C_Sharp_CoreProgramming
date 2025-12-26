7e3d01788b787c2c13db6b71d984b45d7bfdd0a7
jaykant01 <jaykanty24092004@gmail.com>
2025-12-26 15:22:33 +0530

    [Jaykant] Add .FizzBuzz Advanced

---
+++ Review/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿namespace Review;
+
+public class FizzBuzz
+{
+    public void Review1()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n == 0)
+        {
+            Console.WriteLine("Try Again");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        string res = (n % 3 == 0) ? "Fizz" : "Buzz";
+        string res1 = (n % 3 == 0 && n % 5 == 0) ? "FizzBuzz" : res;
+        // Console.WriteLine(res);
+        Console.WriteLine(res1);
+        Console.ReadKey();
+    }
+}
---
+++ Review/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Review;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Review 1
+        Console.WriteLine("Enter the Input Number");
+        FizzBuzz fb = new FizzBuzz();
+        fb.Review1();
+       
+    }
+}
